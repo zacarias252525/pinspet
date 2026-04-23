4f34583b6b7106c997f9787743683425b2035796
zacarias252525 <zacs_8@hotmail.com>
2026-04-23 12:09:43 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.static(".")); // sirve index.html, admin.html, etc
+
+let pedidos = [];
+
+// Crear pedido
+app.post("/pedido", (req, res) => {
+  pedidos.push(req.body);
+  console.log("Nuevo pedido:", req.body);
+  res.send({ ok: true });
+});
+
+// Obtener pedidos
+app.get("/pedidos", (req, res) => {
+  res.send(pedidos);
+});
+
+// Borrar pedidos
+app.delete("/pedidos", (req, res) => {
+  pedidos = [];
+  res.send({ ok: true });
+});
+
+// 🚨 IMPORTANTE PARA RENDER
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
+});
